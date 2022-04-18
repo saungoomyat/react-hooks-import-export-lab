47c21fa73ecd047fa5e0ad94dfc3172a48d17e3c
Saung Oo Myat <saung.myat@gmail.com>
2022-04-17 21:17:57 -0400

    commit tree b5f592b5b8bcff68497184be442dbeb36dbe4139

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Home from "/home/saung/code/labs/phase-2/react-hooks-import-export-lab/src/components/Home.js";
+import NavBar from "/home/saung/code/labs/phase-2/react-hooks-import-export-lab/src/components/NavBar.js";
+import About from "/home/saung/code/labs/phase-2/react-hooks-import-export-lab/src/components/About.js";
+
 
 function App() {
   return (
